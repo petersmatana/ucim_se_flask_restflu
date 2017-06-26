d2da44205c5bfda3d60e27a55f2f6fa707a0998b
Peter Smatana <petersmatana@gmail.com>
2017-06-26 21:19:08 +0200

    lepsi prace s hesly atd...

--- flask_auth/http_basic_auth.py
+++ flask_auth/http_basic_auth.py
@@ -5,13 +5,32 @@
 from functools import wraps
 from flask import request, Response, Flask
 from flask_restful import Resource, Api
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
+
 
 app = Flask(__name__)
 api = Api(app)
 
 
+class User(object):
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+        # self.pw_hash = ""
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+
+
 def check_auth(username, password):
-    return username == 'admin' and password == 'secret'
+    for user in all_users:
+        if user.username == username:
+            return user.check_password(password)
 
 
 def authenticate():
@@ -39,9 +58,17 @@ class SecretApi(Resource):
 
     @requires_auth
     def get(self):
-        return {'api': 'ok'}
+        return {'api': 'ok2'}
 
 api.add_resource(SecretApi, '/api')
 
+all_users = []
+
 if __name__ == '__main__':
+    u1 = User('smonty', 'ahoj')
+    u2 = User('verca', 'cau')
+
+    all_users.append(u1)
+    all_users.append(u2)
+
     app.run(debug=True)
